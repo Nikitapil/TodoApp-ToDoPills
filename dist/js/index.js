b4a0d3abd4aba8c85fc43657c27fabcab6d54fbf
Nikitapil <nikitapiliavets@MacBook-Pro-Nikita.local>
2021-09-05 01:24:26 +0300

    Almost ready project

---
+++ dist/js/index.js
@@ -0,0 +1,167 @@
+//открывание модальных окон
+let modalContainer = document.querySelector(".modal__container")
+function showModal(modal) {
+    modalContainer.classList.toggle("hidden")
+    modal.classList.toggle("hidden")
+}
+let todoContainer = document.querySelector(".todo__container")
+//подгрузка данных из localStarage
+if (localStorage.getItem("elements")) {
+    todoContainer.innerHTML = localStorage.getItem("elements")
+}
+// модальное окно для добавления элемента
+let addButton = document.querySelector(".todo__addBtn");
+let addModal = document.querySelector(".modal__add");
+let addModalCancel = document.querySelector(".modal__cancel__addbtn")
+addButton.addEventListener("click", ()=> showModal(addModal))
+addModalCancel.addEventListener("click", ()=> showModal(addModal))
+//Добавление задачи
+let addItemBtn = document.querySelector(".modal__add__addbtn")
+let titleInput = document.querySelector(".todo__title__input")
+let descriptionInput = document.querySelector(".todo__comment__input")
+let mainBlock = document.querySelector(".todo__main__content")
+addItemBtn.addEventListener("click", addItem);
+function addItem() {
+    let todoItem = document.createElement("div")
+    todoItem.draggable = true
+    let data = new Date()
+    let dataContent = data.getDate() + "." + (data.getMonth()+1) + "." + data.getFullYear()
+    todoItem.classList.add("todo__item")
+    todoItem.innerHTML = `<h4 class="todo__item__header">${titleInput.value}</h4>
+    <div class="todo__item__buttons">
+        <button class="inprogress__button" title="В процессе"></button>
+        <button class="delete__button" title="Удалить"></button>
+        <button class="inmain__button" title="В невыполненные"></button>
+        <button class="indone__button" title="В выполненные"></button>
+    </div>
+    <p class="todo__item__description">${descriptionInput.value}</p>
+    <p class="todo__item__date">${dataContent}</p>
+    <button class="item__show"><span>Показать</span></button>`
+    mainBlock.append(todoItem);
+    showModal(addModal)
+    localStorage.setItem("elements", todoContainer.innerHTML)
+    counters()
+}
+//переброс элемента в в другие блоки 
+let inprogressBlock = document.querySelector(".todo__inprogress__content")
+let doneBlock = document.querySelector(".todo__done__content")
+let trashBlock = document.querySelector(".todo__trash__content")
+let deleteModal = document.querySelector(".modal__delete")
+let delCancelBtn = document.querySelector(".modal__delete__cancelbtn")
+delCancelBtn.addEventListener("click", ()=> showModal(deleteModal))
+todoContainer.addEventListener("click", function (event){
+    if (event.target.classList.contains("inprogress__button")) {
+
+        changeBlock (event.target, inprogressBlock)
+    }
+    else if (event.target.classList.contains("inmain__button")) {
+        changeBlock (event.target, mainBlock)
+    }
+    else if (event.target.classList.contains("indone__button")) {
+        changeBlock (event.target, doneBlock)
+    }
+    else if (event.target.classList.contains("delete__button")) {
+        if (event.target.closest(".todo__item").parentElement.classList.contains("todo__trash__content")){
+            showModal(deleteModal)
+            let delBtn = document.querySelector(".modal__delete__deletebtn")
+            delBtn.onclick = function () {
+            event.target.closest(".todo__item").remove()
+            localStorage.setItem("elements", todoContainer.innerHTML)
+            counters()
+            showModal(deleteModal)
+            }
+        }
+        else {
+            changeBlock (event.target, trashBlock)
+        }
+    }
+    else if (event.target.classList.contains("todo__clear")) {
+        event.target.closest(".todo__block").querySelector(".todo__content").innerHTML =""
+        localStorage.setItem("elements", todoContainer.innerHTML)
+            counters()
+    }
+})
+
+function changeBlock (button, block) {
+    let item = button.closest(".todo__item")
+    block.append(item)
+    localStorage.setItem("elements", todoContainer.innerHTML)
+    counters()
+}
+// drag'n'drop
+let todoBlocks = document.querySelectorAll(".todo__block")
+
+todoBlocks.forEach((item)=>{
+    item.addEventListener("dragstart", function (event) {
+        event.target.classList.add("dragnow");
+      });
+      item.addEventListener("dragend", function (event) {
+        event.target.classList.remove("dragnow");
+      });
+      item.addEventListener("dragover", function (event) {
+        event.preventDefault();
+      });
+      item.addEventListener("drop", function (event) {
+        event.preventDefault();
+        let dragTodo = document.querySelector(".dragnow");
+        if (dragTodo != null) {
+            let contentBlock = item.querySelector(".todo__content")
+            contentBlock.append(dragTodo);
+        }
+        counters()
+      });
+})
+//счетчики
+function counters() {
+    todoBlocks.forEach((item)=>{
+        
+          let counter = item.querySelector(".todo__counter")
+          let contentBlock = item.querySelector(".todo__content")
+          counter.textContent = contentBlock.children.length
+})
+}
+counters()
+// открытие и редактирование задачи
+let editModal = document.querySelector(".modal__edit")
+
+todoContainer.addEventListener("click", function (event){
+if (event.target.classList.contains("item__show")) {
+    let itemTitle = event.target.closest(".todo__item").querySelector(".todo__item__header").textContent
+    let itemdescription = event.target.closest(".todo__item").querySelector(".todo__item__description").textContent
+    editModal.innerHTML = `
+    <h2>${itemTitle}</h2>
+            <p>${itemdescription}</p>
+            <div class="modal__edit__buttons">
+                <button type="button" class="modal__edit__editbtn">Редактировать</button>
+                <button type="button" class="modal__cancel__editbtn">Закрыть</button>
+            </div>
+    `
+    let cancelEditModal = document.querySelector(".modal__cancel__editbtn")
+    cancelEditModal.addEventListener("click", ()=> showModal(editModal))
+    let editBtn = document.querySelector(".modal__edit__editbtn")
+    editBtn.onclick = function () {
+        editModal.innerHTML = `
+        <label for="edit__header">Название</label>
+    <input type="text" value="${itemTitle}" id="edit__header">
+    <label for="edit__comment">Описание</label>
+            <textarea id="edit__comment">${itemdescription}</textarea>
+            <div class="modal__edit__buttons">
+                <button type="button" class="modal__save__editbtn">Сохранить</button>
+                <button type="button" class="modal__cancel__editbtn">Закрыть</button>
+            </div>
+    `
+    let cancelEditModal = document.querySelector(".modal__cancel__editbtn")
+    cancelEditModal.addEventListener("click", ()=> showModal(editModal))
+    let editHeaderInput = document.getElementById("edit__header")
+    let editCommentInput = document.getElementById("edit__comment")
+    let savelEditModal = document.querySelector(".modal__save__editbtn")
+    savelEditModal.addEventListener("click", function(){
+        event.target.closest(".todo__item").querySelector(".todo__item__header").textContent = editHeaderInput.value
+        event.target.closest(".todo__item").querySelector(".todo__item__description").textContent = editCommentInput.value
+        localStorage.setItem("elements", todoContainer.innerHTML)
+        showModal(editModal)
+    })
+    }
+    showModal(editModal)
+}
+})
---
+++ gulpfile.js
@@ -0,0 +1,212 @@
+let project_folder = "dist";
+let source_folder = "src";
+
+let path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    favicon: project_folder + "/",
+    fonts: project_folder + "/fonts/",
+    audio: project_folder + "/audio/",
+  },
+  src: {
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+    css: source_folder + "/scss/style.scss",
+    js: source_folder + "/js/*.js",
+    img: [source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}"],
+    favicon: source_folder + "/*.png",
+    fonts: source_folder + "/fonts/*.ttf",
+    audio: [source_folder + "/audio/*.{mp3,ogg,mp4}"],
+  },
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    audio: source_folder + "/audio/*.{mp3,ogg}",
+  },
+  clean: "./" + project_folder + "/",
+};
+
+let { src, dest } = require("gulp"),
+  gulp = require("gulp"),
+  browsersync = require("browser-sync").create(),
+  fileinclude = require("gulp-file-include"),
+  del = require("del"),
+  scss = require("gulp-sass")(require("sass")),
+  autoprefixer = require("gulp-autoprefixer"),
+  group_media = require("gulp-group-css-media-queries"),
+  clean_css = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify-es").default,
+  imagemin = require("gulp-imagemin"),
+  webp = require("gulp-webp"),
+  webphtml = require("gulp-webp-html"),
+  /* webpcss = require("gulp-webp-css"), */
+  svgSprite = require("gulp-svg-sprite"),
+  ttf2woff = require("gulp-ttf2woff"),
+  ttf2woff2 = require("gulp-ttf2woff2"),
+  fonter = require("gulp-fonter");
+prettyHtml = require("gulp-pretty-html");
+
+function browserSync(params) {
+  browsersync.init({
+    server: {
+      baseDir: "./" + project_folder + "/",
+    },
+    port: 3000,
+    notify: false,
+  });
+}
+
+function js() {
+  return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        extname: ".min.js",
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream());
+}
+
+function html() {
+  return (
+    src(path.src.html)
+      .pipe(fileinclude())
+      .pipe(webphtml())
+      .pipe(prettyHtml({
+      unformatted: ['code', 'pre', 'em', 'strong', 'span', 'i', 'b', 'br'],
+      indent_inner_html: false,
+    }))
+      .pipe(dest(path.build.html))
+      .pipe(browsersync.stream())
+  );
+}
+
+
+function css() {
+  return (
+    src(path.src.css)
+      .pipe(
+        scss({
+          outputStyle: "expanded",
+        })
+      )
+      .pipe(group_media())
+      .pipe(
+        autoprefixer({
+          overrideBrowserslist: ["last 5 versions"],
+          cascade: true,
+        })
+      )
+      /* .pipe(webpcss()) */
+      .pipe(dest(path.build.css))
+      .pipe(clean_css())
+      .pipe(
+        rename({
+          extname: ".min.css",
+        })
+      )
+      .pipe(dest(path.build.css))
+      .pipe(browsersync.stream())
+  );
+}
+
+function clean(params) {
+  return del(path.clean);
+}
+
+function images() {
+  return src(path.src.img)
+    .pipe(
+      webp({
+        quality: 70,
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        interlaced: true,
+        optimizationlevel: 3,
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream());
+}
+function favicon() {
+  return (src(path.src.favicon)
+      .pipe(dest(path.build.favicon))
+      .pipe(browsersync.stream())
+  );
+}
+function audio () {
+  return (src(path.src.audio)
+      .pipe(dest(path.build.audio))
+      .pipe(browsersync.stream())
+  );
+}
+function fonts(params) {
+  src(path.src.fonts)
+  .pipe(ttf2woff())
+  .pipe(dest(path.build.fonts));
+  return src(path.src.fonts)
+  .pipe(ttf2woff2())
+  .pipe(dest(path.build.fonts));
+}
+
+gulp.task("otf2ttf", function () {
+  return src([source_folder + "/fonts/*.otf"])
+    .pipe(
+      fonter({
+        formats: ["ttf"],
+      })
+    )
+    .pipe(dest(source_folder + "/fonts/"));
+});
+
+gulp.task("svgSprite", function () {
+  return gulp
+    .src([source_folder + "/iconsprite/*.svg"])
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../icons/icons.svg", //sprite file name
+            example: true,
+          },
+        },
+      })
+    )
+    .pipe(dest(path.build.img));
+});
+
+function watchFiles(params) {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
+  gulp.watch([path.watch.audio], audio);
+}
+
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts, favicon, audio));
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.audio = audio;
+exports.favicon = favicon;
+exports.html = html;
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
+exports.css = css;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
